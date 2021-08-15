a1039095640c55b63e4671d5e1bda8a00b1f9cec
Mckevin11 <56427414+Mckevin11@users.noreply.github.com>
2021-08-15 18:38:13 +0800

    Add files via upload

---
+++ WebDevelopment/Code/my-project/public/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <title>StartApp</title>
+        <link href="main.css" rel="stylesheet">
+    </head>
+
+    <body>
+      <div class="webname"> <a href="Maks.html">StartApp</a></div>
+
+        <h1 class="header">
+
+          <li> <a href="Page2.html">Home</a></li>
+          <li> <a href="#About">About</a></li>
+          <li> <a href="#">Blog</a></li>
+          <li> <a href="#">Contact</a></li>
+          <ul> <a href="#Map">Location</a></ul>
+
+        </h1>
+
+      <div id="About">
+
+      <h2 class="About">
+       About
+        <p>
+      </p>
+    </h2>
+      </div>
+
+      <div id="Map">
+      <div class="footer">
+        <ul>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63142.70415158299!2d124.60966795989452!3d8.458569941717712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32ff8d2656976bdf%3A0x96ec1f263139b3b9!2sTierra%20de%20Oro%20Homes%2C%20Mandumol-Aluba-Buena%20Oro%20Rd%2C%20Cagayan%20de%20Oro%2C%209000%20Misamis%20Oriental!5e0!3m2!1sen!2sph!4v1628910549333!5m2!1sen!2sph"
+        width="1300"
+        height="500"
+        style="border:0;"
+        allowfullscreen=""
+        loading="lazy">
+      </iframe>
+      </ul>
+      </div>
+    </div>
+
+      </body>
+</html>
